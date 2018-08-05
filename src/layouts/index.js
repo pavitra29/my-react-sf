3a7b41464cc5fbeb6a9c32f59b6502a8a4f4c5d6
Pavitra Srinivasan <pavitra_srinivasan@apple.com>
2018-08-05 15:21:19 -0700

    case error

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/header'
 import './index.css'
-import Footer from '../components/footer';
+import Footer from '../components/Footer';
 
 const Layout = ({ children, data }) => (
   <div>
